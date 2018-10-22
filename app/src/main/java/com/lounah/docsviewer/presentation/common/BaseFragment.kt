2ef2646aaea7c0d88a8f772f4036e16b2fcbf0ab
Lounah <lounahxrussia@gmail.com>
2018-10-22 14:03:11 +0300

    added di into a base fragment

--- app/src/main/java/com/lounah/docsviewer/presentation/common/BaseFragment.kt
+++ app/src/main/java/com/lounah/docsviewer/presentation/common/BaseFragment.kt
@@ -1,12 +1,19 @@
 package com.lounah.docsviewer.presentation.common
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import dagger.android.support.AndroidSupportInjection
 
 abstract class BaseFragment : Fragment() {
+
+    override fun onAttach(context: Context?) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View?
             = inflater.inflate(layoutRes, container, false)
